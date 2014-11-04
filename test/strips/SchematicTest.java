4691adbd6ee5cd8974ffe1b765a4293dfaf4627c
David Capper <dmc2@aber.ac.uk>
2014-11-04 13:52:00 +0000

    Started adding schematic

---
+++ test/strips/SchematicTest.java
@@ -0,0 +1,11 @@
+package strips;
+
+import junit.framework.TestCase;
+
+public class SchematicTest extends TestCase {
+
+    public void testNewSchematic() throws Exception {
+        assertTrue(false);
+
+    }
+}
